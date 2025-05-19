22852a6abe59999a8ed36355dbcdfcbcfd556c29
unknown <Administrator@educauca.gov.co>
2025-05-19 17:13:00 -0500

    Actualiza TicketListScreen.js

--- src/screens/TicketListScreen.js
+++ src/screens/TicketListScreen.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import TicketList from '../components/TicketList';
 
+const TicketListScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista de Tickets</Text>
+      <TicketList />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  // ... tus estilos aquí ...
+});
+
+export default TicketListScreen;
